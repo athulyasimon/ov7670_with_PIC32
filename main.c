9b9cf58ab6821fedc22ba600bd891f360c7532c7
Athulya Simon <AthulyaSimon2014@u.northwestern.edu>
2015-12-11 00:37:43 -0600

    updated code

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 //#define NU32_STANDALONE  // uncomment if program is standalone, not bootloaded
 #include "NU32.h"          // config bits, constants, funcs for startup and UART
-#include "i2c.h"
+// #include "i2c.h"
 #include "NU32_I2C.h"
 
 #define MSG_LEN 20
-#define FULL_DUTY 6000 //About 10 MHz
+#define FULL_DUTY 3000 //About 10 MHz
 #define ADC_I2C_BAUD  9600
 
 #define HREF PORTBbits.RB10
-#define PCLK PORTBbits.RB9
+#define RCK PORTBbits.RB9 
+
+#define WRST PORTEbits.RE8 
+
+
+//SIOC - A2 (SCL2)
+//SIOD - A3 (SDA2)
+//VSYNC - F5 (CN18)
 
 /******************************************************************************
 * HELPER FUNCTION PROTOTYPES
@@ -19,7 +26,6 @@ char read_byte();
 void data_config();
 void CNinitialize();
 void camera_reset();
-void write_reset();
 void delay();
 
 /******************************************************************************
@@ -34,46 +40,42 @@ int j = 0;
 int h = 0;
 unsigned char pixel;
 unsigned char data[100];
+int read_state = 0;
+
+int count = 0;
 /**********************************************************************
 * Interrupt(s)
 *********************************************************************/
 void __ISR(_CHANGE_NOTICE_VECTOR, IPL3SOFT) CNISR(void) { // INT step 1
 	newF = PORTF; // since pins on port F are being monitored by CN,
 				  // must read both to allow continued functioning
-	write_reset();
-	LATESET = 0b100000000;
 
-	CNCONbits.ON = 0; // Temporarily turn CN pin off
+	count++;
+
+	// CNCONbits.ON = 0; // Temporarily turn CN pin off
 	// NU32_LED1 = !NU32_LED1; // toggle LED1
-	IFS1bits.CNIF = 0; // clear the interrupt flag
 	NU32_WriteUART1("interrupt \r\n");
 
-	while(rows < 144){
-		if(HREF){
-			while(HREF){
-				if(PCLK && columns <= 174){
-					img_array[rows][columns] = read_byte();
-					columns++;
-				}
-				columns = 0;
-				rows++;
-			}
-		}
+	read_state = !read_state;
+
+	if(read_state){
+		LATECLR = 0b100000000; //reset write pointer to 0 which is the beginning of the frame
+		LATESET = 0b1100000000; //writer pointer and set FIFO Write enable to high
+
 	}
 
-	for(j=0; j<144; j++){
-		for(i=0; i<1; i++){
-			pixel = img_array[j][i];
-			sprintf(data, "%d", pixel);
-			NU32_WriteUART1(data);
-			NU32_WriteUART1(" ");
-		}
-		
+	if(!read_state){
+		LATECLR = 0b1000000000; //set FIFO write enable to low
 	}
 
-	columns = 0;
-	rows = 0;	
+	if(count >= 2){
+		CNCONbits.ON = 0; // Temporarily turn CN pin off
+		NU32_WriteUART1("image stored");
+	}
+	
 	NU32_WriteUART1("\n");
+	IFS1bits.CNIF = 0; // clear the interrupt flag
+
 }
 
 /******************************************************************************
@@ -88,18 +90,24 @@ int main(){
 	NU32_Startup();
 
 	__builtin_disable_interrupts();
+	// INTDisableInterrupts();
 
-	i2c_initialize(ADC_I2C_BAUD);
+	// i2c_initialize(ADC_I2C_BAUD);
+	I2Cinitialize(SLOW_BAUD_RATE);
 	xclkInitialize();
 	data_config();
 	CNinitialize();
 
   	__builtin_enable_interrupts();
+	// INTEnableSystemMultiVectoredInt();
 
   	//Camera settings
-	i2c_write(0x42, 0x11, 0b10000011); //pclk is 32 times xclk period
-	i2c_write(0x42, 0x0c, 0b00001000); //Enable scaling
-	i2c_write(0x42, 0x12, 0b00001000); //QCIF format
+	// i2c_write(0x42, 0x11, 0b10000011); //pclk is 32 times xclk period
+	// i2c_write(0x42, 0x0c, 0b00001000); //Enable scaling
+	// i2c_write(0x42, 0x12, 0b00001000); //QCIF format
+	I2Cwrite(0x42, 0x11, 0b10000011); //pclk is 32 times xclk period
+	I2Cwrite(0x42, 0x0c, 0b00001000); //Enable scaling
+	I2Cwrite(0x42, 0x12, 0b00001000); //QCIF format
 
   	while(1){
   		NU32_WriteUART1("Press 'r' to read a byte \r\n");
@@ -116,6 +124,55 @@ int main(){
     			sprintf(data, "%d",buffer);
     			NU32_WriteUART1(data);
     			break;
+    		}
+
+    		case 'i':
+    		{
+    			//read image
+    			//Read reset - pin F0
+    			LATFCLR = 0b1; //Initialize read address to 0
+    			delay();
+    			delay();
+    			delay();
+    			LATFSET = 0b1; // Return RRST to high 
+				
+				//Output enable - pin F1
+				LATFCLR = 0b10; //set pin low
+
+				//Read out image
+				while(rows < 144){
+					if(HREF){
+						while(HREF){
+							if(RCK && columns <=174){
+								img_array[rows][columns] = read_byte();
+								columns ++;
+								while(RCK){
+									{}; //do nothing
+								}
+							}
+							columns = 0;
+							rows ++;
+						}
+					}
+				}
+
+				LATFSET = 0b10; //set pin high
+
+				//Print out image
+				for(j=0; j<144; j++){
+					for(i=0; i<5; i++){
+						pixel = img_array[j][i];
+						sprintf(data, "%d", pixel);
+						NU32_WriteUART1(data);
+						NU32_WriteUART1(" ");
+					}
+				}
+
+				columns = 0;
+				rows = 0;
+				NU32_WriteUART1("\n");
+				break;
+
     		}
 
     		case 'h':
@@ -137,18 +194,24 @@ int main(){
 
     		case 's':
     		{
-			    i2c_startevent();
-				// i2c_sendonebyte(0x42);
-				// i2c_sendonebyte(0x01);
+			    delay();
+			 //    i2c_startevent();
+				// // // i2c_sendonebyte(0x42);
+				// // // i2c_sendonebyte(0x01);
 
-			 //    i2c_repeatstartevent();
-				i2c_sendonebyte(0x43);
-				*value_ptr = i2c_readonebyte(0x01);
-				i2c_stopevent();
+			 // // //    i2c_repeatstartevent();
+				// // // i2c_sendonebyte(0x43);
+				// delay();
+				// // value_ptr = i2c_readonebyte(0x01);
+				// i2c_stopevent();
+				// delay();
 				// value_ptr = &value;
-				// I2Cread(0x42, 0x01, value_ptr);
-    			// i2c_read(0x42, 0x0c, value_ptr);
-    			sprintf(data, "%d", value_ptr);
+				I2Cread(0x42, 0x01, value_ptr);
+    			// i2c_read(0x42, 0x01, value_ptr);
+    			delay();
+    			delay();
+    			delay();
+    			sprintf(data, "%d", *value_ptr);
     			NU32_WriteUART1(data);
     			NU32_WriteUART1("\r\n");
     			break;
@@ -168,7 +231,11 @@ void data_config() {
 	LATDbits.LATD3 = 0;	  //0 sets output to low
 	TRISEbits.TRISE9 = 0; //0 = output
 	TRISEbits.TRISE8 = 0;
+	TRISFbits.TRISF0 = 0; //0 = output
+	TRISFbits.TRISF1 = 0;
 	LATESET = 0b110000000; //set pins 8 and 9 high
+	LATFSET = 0b11; //set pin 0 and 1 high
+	// LATFCLR = 0b10; //set pin 1 low
 	// LATEbits.LATE3 = 1;   //1 sets output to floating
 }
 
@@ -207,13 +274,6 @@ void CNinitialize(){
 void camera_reset(){
 	LATEbits.LATE9 = 0; //0
 	LATEbits.LATE9 = 1; //1
-}
-
-void write_reset(){
-	LATECLR = 0b100000000;
-	delay();
-	delay();
-	delay();
 }
 
 
